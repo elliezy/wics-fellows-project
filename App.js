7cc51bb69637b32797786e5773b5d4128e391885
ellie <elliezy@stanford.edu>
2022-04-26 20:30:01 -0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import MovieItem from "./MovieItem";
 
 export default function App() {
+  const [movies, setMovies] = useState([]);
+
+  const getMoviesFromApiAsync = async () => {
+    try {
+      const response = await fetch(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=d98e9470bb9d406d7060f45b4ce7ef3c'
+      );
+      const json = await response.json();
+      setMovies(json.results);
+      // console.log("===============================================================================");
+      // console.log("here are my movies");
+      console.log(movies);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getMoviesFromApiAsync();
+  }, []);
+
+  const renderItem = (item) => (
+    <MovieItem 
+      name={item.original_title}
+      imageURL={"http://image.tmdb.org/t/p/w500" + item.poster_path}
+      description={item.overview}
+    />
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.logo}>Popcorn 🍿</Text>
+      <FlatList 
+        data={movies}
+        renderItem={({item}) => renderItem(item)}
+        keyExtractor={(item) => item.id}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'black',
+    // alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    color: 'red',
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: "3%",
+    marginLeft: "1%",
   },
 });
---
+++ MovieItem.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Text, TextInput, View, StyleSheet, Dimensions, Image } from 'react-native';
+
+const MovieItem = ({name, imageURL, description}) => {
+  return (
+    <View style={styles.item}>
+        <View style={styles.imageView}>
+            <Image style={styles.image} source={{uri: imageURL}}/>
+        </View>
+        <View style={styles.info}>
+            <Text style={styles.title}>{name}</Text>
+            <Text numberOfLines={6} style={styles.text}>{description}</Text>
+        </View>
+      
+    </View>
+  );
+}
+
+export default MovieItem;
+
+const styles = StyleSheet.create({
+    item: {
+        display: "flex",
+        flex: 1,
+        flexDirection: "row",
+        // justifyContent: "center",
+        // alignItems: "center",
+        width: Dimensions.get('window').width,
+        marginBottom: "3%",
+        paddingLeft: "1%",
+        paddingRight: "1%",
+    },
+    image: {
+        width: 80,
+        height: 120,
+        margin: 5,
+        display: "flex",
+        resizeMode: "contain",
+        flex: 1,
+        marginLeft: "3%",
+        marginRight: "3%",
+    },
+    imageView: {
+        display: "flex",
+        flex: 1,
+    },
+    text: {
+        color: "white",
+        fontSize: 11,
+        // fontFamily: "AvenirNext-Regular"
+    },
+    title: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 16,
+        marginBottom: "1%",
+    },
+    info: {
+        display: "flex",
+        flex: 3,
+    }
+
+});
